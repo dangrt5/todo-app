6d8eceec64b52f312aeadf8917a38ace16e6cdb5
dangrt5 <randy.dang05@gmail.com>
2018-09-25 14:33:42 -0700

    todo list routing set up

--- src/components/app.js
+++ src/components/app.js
@@ -1,75 +1,17 @@
 import "materialize-css/dist/css/materialize.min.css";
+import {Route} from "react-router-dom";
 import React, {Component} from 'react';
-import axios from "axios";
 import List from "./list";
 import AddItem from "./add-item";
-import dummyListData from "../dummy-data/list-data";
 
-const BASE_URL = "http://api.reactprototypes.com";
-const API_KEY = "?key=rvndydvng"
 
 class App extends Component {
-  state = {
-    list: [],
-    error: ""
-  }
-
-  componentDidMount() {
-    this.getListData();
-  }
-
-  async getListData() {
-      // Call server to get data
-      // http://api.reactprototypes.com/todos?key=somekey
-
-      try {
-        const response = await axios.get(`${BASE_URL}/todos${API_KEY}`);
-        this.setState({
-          list: response.data.todos
-        });
-      } catch(error) {
-          this.setState({
-            error: "Error retrieving list data"
-          })
-        }
-
-
-            // const response = axios.get(`${BASE_URL}/todos${API_KEY}`).then((response) => {
-      //   this.setState({
-      //     list: response.data.todos
-      //   });
-      // }).catch((err) => {
-      //   console.log("Add Item Error", err.message);
-      //   this.setState({
-      //     error: `Error retrieving list data`
-      //   })
-      // })
-
-
-
-  }
-
-  async addItem(item) {
-    await axios.post(`${BASE_URL}/todos${API_KEY}`, item);
-    this.getListData();
-  }
-
-  deleteItem = async (id) => {
-    console.log("Delete item ID:", id);
-    await axios.delete(`${BASE_URL}/todos/${id + API_KEY}`);
-    this.getListData();
-  }
-
 
   render() {
-    const {list, error} = this.state;
-    console.log("List", list)
     return (
         <div className="container">
-          <h1 className="center">To Do App</h1>
-          <AddItem add={this.addItem.bind(this)}/>
-          <p className="red-text">{error}</p>
-          <List data={list} delete={this.deleteItem}/>
+          <Route exact path="/" component={List}/>
+          <Route path="/add-item" component={AddItem}/>
         </div>
       )
   }
--- src/components/list.js
+++ src/components/list.js
@@ -1,13 +1,70 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Item from './item';
+import NavBtn from "./nav_btn";
+import config from "../config";
+import axios from "axios";
 
-export default props => {
-  const listElements = props.data.map((item, index)=>{
-    return <Item key={item._id} item={item} index={index} delete={() => props.delete(item._id)}/>
-  })
-  return(
-    <ul className="collection">
-      {listElements}
-    </ul>
-  )
+class List extends Component {
+  state = {
+    list: [],
+    error: ""
+  }
+
+  componentDidMount() {
+    this.getListData();
+  }
+
+  async getListData() {
+
+
+      try {
+        const response = await axios.get(`${config.API_URL}/todos${config.API_KEY}`);
+        this.setState({
+          list: response.data.todos
+        });
+      } catch(error) {
+          this.setState({
+            error: "Error retrieving list data"
+          })
+        }
+
+
+            // const response = axios.get(`${BASE_URL}/todos${API_KEY}`).then((response) => {
+      //   this.setState({
+      //     list: response.data.todos
+      //   });
+      // }).catch((err) => {
+      //   console.log("Add Item Error", err.message);
+      //   this.setState({
+      //     error: `Error retrieving list data`
+      //   })
+      // })
+
+  }
+
+  deleteItem = async (id) => {
+    console.log("Delete item ID:", id);
+    await axios.delete(`${config.API_URL}/todos/${id + config.API_KEY}`);
+    this.getListData();
+  }
+
+  render() {
+    const {error, list} = this.state;
+      const listElements = list.map((item, index)=>{
+        return <Item key={item._id} item={item} index={index} delete={() => this.deleteItem(item._id)}/>
+      })
+      return (
+        <div>
+          <h1 className="center">To Do List</h1>
+          <NavBtn to="/add-item" color="green darken-2" text="Add Item"/>
+          <p className="red-text text-darken-2">{error}</p>
+          <ul className="collection">
+            {listElements}
+          </ul>
+        </div>
+      )
+    }
 }
+
+
+export default List;
